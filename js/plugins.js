f5b138473d1615af9cae5a70dbc310be560e9636
jkrumm <jkrumm@pm.me>
2022-09-11 14:21:20 +0200

    minor seo improvement & typo

--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,8 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+(function () {
   var method;
-  var noop = function () {};
+  var noop = function () {
+  };
   var methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
@@ -20,5 +21,44 @@
     }
   }
 }());
+
+(function () {
+  if (typeof EventTarget !== "undefined") {
+    let func = EventTarget.prototype.addEventListener;
+    EventTarget.prototype.addEventListener = function (type, fn, capture) {
+      this.func = func;
+      if (typeof capture !== "boolean") {
+        capture = capture || {};
+        capture.passive = false;
+      }
+      this.func(type, fn, capture);
+    };
+  }
+}());
+
+// // Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = {lat: -25.344, lng: 131.031};
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+// }
+//
+// window.initMap = initMap;
+
+console.log = function () {
+};
+console.warn = function () {
+};
+console.error = function () {
+};
 
 // Place any jQuery/helper plugins in here.
